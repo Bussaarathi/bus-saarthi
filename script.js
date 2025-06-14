ccc4d545faaf2f8888f8ee03702310a8f49ac198
Bussaarathi <garidepalli5@gmail.com>
2025-06-14 10:21:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("bookingForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const from = document.getElementById("from").value;
-  const to = document.getElementById("to").value;
-  const date = document.getElementById("date").value;
-
-  document.getElementById("result").textContent =
-    `Bus booked from ${from} to ${to} on ${date}!`;
-});
